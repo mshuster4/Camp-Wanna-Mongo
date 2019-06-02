db7cca2aa7e883f717c30878455bf36f2fea3a38
Calberg89 <chris_alberg@yahoo.com>
2019-06-02 13:22:17 -0700

    Added images for Memory game

---
+++ client/src/pages/Resources.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Resources from "../components/Resources";
+
+class Resources extends Component {
+    render() {
+        return (
+            <div>
+                <Resources />
+            </div>
+        );
+    }
+}
+
+export default Resources;
